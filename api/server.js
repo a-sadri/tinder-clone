c529d0803b5149a74a650bc4969aa6b5b3d27296
Ali <asadri721@gmail.com>
2021-09-18 20:54:58 +0430

    Cards model created and card post endpoint is incomplete

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import Cards from './Cards';
 
 // App Config
 const app = express();
@@ -12,12 +13,16 @@ const connection_url =
 // DB Config
 mongoose.connect(connection_url, {
   useNewUrlParser: true,
-  //   useCreateIndex: true,
   useUnifiedTopology: true,
 });
 
 // API Endpoints
 app.get('/', (req, res) => res.status(200).send('Hello express'));
+
+app.post('/tinder/card', (req, res) => {
+  const dbCard = req.body;
+  Cards.create(dbCard, (err, data) => {});
+});
 
 // listener
 app.listen(port, () => console.log(`Listening on localhost: ${port}`));
