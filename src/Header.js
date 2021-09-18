8e7f580fa860230ba63e65856ada68ddfcbb76c5
Ali <asadri721@gmail.com>
2021-09-18 16:00:41 +0430

    Header and Card components was added

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import PersonIcon from '@material-ui/icons/Person';
 import IconButton from '@material-ui/core/IconButton';
+import ForumIcon from '@material-ui/icons/Forum';
 import './header.css';
 
 export default function Header() {
@@ -9,6 +10,10 @@ export default function Header() {
         <PersonIcon fontSize='large' className='header__icon' />
       </IconButton>
       <img className='header__logo' src='/images/logo.svg' />
+
+      <IconButton>
+        <ForumIcon fontSize='large' className='header__icon' />
+      </IconButton>
     </div>
   );
 }
